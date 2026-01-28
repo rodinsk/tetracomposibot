e5e6faf39b5602e541116d6d1c8886c80327c40a
rodinsk <rmpstime@gmail.com>
2026-01-28 18:27:56 +0100

    suite

--- config_TP1.py
+++ config_TP1.py
@@ -5,9 +5,9 @@ import arenas
 # general -- first three parameters can be overwritten with command-line arguments (cf. "python tetracomposibot.py --help")
 
 display_mode = 0
-arena = 1
+arena = 2
 position = False 
-max_iterations = 501 #401*500
+max_iterations = 2000 #401*500
 
 # affichage
 
@@ -21,10 +21,13 @@ display_time_stats = True
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
 import robot_dumb
+import robot_braitenberg_avoider as robotavoider
+import robot_braitenberg_loveWall as robotlovewall
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    robots.append(robotavoider.Robot_player(4, y_center, 0, name="Avoider", team="A"))
+    robots.append(robotlovewall.Robot_player(80, y_center, 0, name="LoverWall", team="A"))
     return robots
