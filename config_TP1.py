d6a6240ef8ac1742a3bbd0fcc330ef18cb52c156
Hongshuo22 <hongshuo75@gmail.com>
2026-01-28 22:41:22 +0100

    hateBot

--- config_TP1.py
+++ config_TP1.py
@@ -5,7 +5,7 @@ import arenas
 # general -- first three parameters can be overwritten with command-line arguments (cf. "python tetracomposibot.py --help")
 
 display_mode = 0
-arena = 3
+arena = 1
 position = False 
 max_iterations = 2000 #401*500
 max_iterations = 2001 #401*500
@@ -27,6 +27,7 @@ import robot_braitenberg_loveWall as robotlovewall
 import robot_braitenberg_avoider
 import robot_braitenberg_hateWall
 import robot_braitenberg_loveBot
+import robot_braitenberg_hateBot
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
@@ -34,4 +35,8 @@ def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of t
     robots = []
     robots.append(robot_braitenberg_avoider.Robot_player(24, y_center, 0, name="My Robot", team="A"))
     robots.append(robot_braitenberg_loveBot.Robot_player(93, y_center, 0, name="My Robot", team="A"))
+    robots.append(robot_braitenberg_hateBot.Robot_player(70, y_center, 0, name="My Robot", team="A"))
+    robots.append(robot_braitenberg_loveBot.Robot_player(75, y_center-20, 0, name="My Robot", team="A"))
+    robots.append(robot_braitenberg_loveBot.Robot_player(85, y_center+20, 0, name="My Robot", team="A"))
+    robots.append(robot_dumb.Robot_player(80, y_center, 0, name="My Robot", team="A"))
     return robots
