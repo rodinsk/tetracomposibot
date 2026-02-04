e7ac60855bb49316342f8c55132c0d796fd0c4df
Hongshuo22 <hongshuo75@gmail.com>
2026-02-04 15:57:31 +0100

    resultat test

--- config_Paintwars.py
+++ config_Paintwars.py
@@ -23,6 +23,7 @@ display_time_stats = False
 import robot_challenger
 import robot_champion
 import robot_subsomption
+import robot_braitenberg_avoider
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     global position
--- plot_resultat.py
+++ plot_resultat.py
@@ -13,11 +13,29 @@ def read_data(filename):
     return iterations, best_scores
 
 
-iter_rand, score_rand = read_data("resultat_test/resultat_random_1.txt") 
+iter_rand1, score_rand1= read_data("resultat_test/resultat_random_1.txt")
+iter_rand2, score_rand2 = read_data("resultat_test/resultat_random_2.txt")
+iter_rand3, score_rand3 = read_data("resultat_test/resultat_random_3.txt")
+iter_rand4, score_rand4 = read_data("resultat_test/resultat_random_4.txt")
+iter_rand5, score_rand5 = read_data("resultat_test/resultat_random_5.txt")
 iter_gen, score_gen = read_data("resultat_test/resultat_algo_1.txt")
+iter_gen2, score_gen2 = read_data("resultat_test/resultat_algo_2.txt")
+iter_gen3, score_gen3 = read_data("resultat_test/resultat_algo_3.txt")
+iter_gen4, score_gen4 = read_data("resultat_test/resultat_algo_4.txt")
+iter_gen5, score_gen5 = read_data("resultat_test/resultat_algo_5.txt")
 
-plt.plot(iter_rand, score_rand, label="Recherche Aléatoire", color="blue")
-plt.plot(iter_gen, score_gen, label="Algorithme Génétique", color="red")
+plt.plot(iter_rand1, score_rand1, label="Recherche Aléatoire ", color="orange")
+plt.plot(iter_rand2, score_rand2, color="orange")
+plt.plot(iter_rand3, score_rand3, color="orange")
+plt.plot(iter_rand4, score_rand4, color="orange")
+plt.plot(iter_rand5, score_rand5, color="orange")
+
+plt.plot(iter_gen, score_gen, label="Algorithme Génétique ", color="purple")
+plt.plot(iter_gen2, score_gen2, color="purple")
+plt.plot(iter_gen3, score_gen3, color="purple")
+plt.plot(iter_gen4, score_gen4, color="purple")
+plt.plot(iter_gen5, score_gen5, color="purple")
+
 
 
 plt.title("Comparaison : Hasard vs Génétique")
