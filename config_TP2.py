ba1e64d294cee0250f280b2c825a0aebe6e07bfc
rodinsk <rmpstime@gmail.com>
2026-02-04 02:10:24 +0100

    ex3

--- config_TP2.py
+++ config_TP2.py
@@ -29,10 +29,11 @@ max_iterations = evaluations * it_per_evaluation + 1
 import robot_optimize
 import robot_randomsearch
 import robot_randomsearch2
+import genetic_algorithms
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_randomsearch2.Robot_player(x_center, y_center, 0, name="My Robot", team="A",evaluations=evaluations,it_per_evaluation=it_per_evaluation)) # start from left: 4, y_center
+    robots.append(genetic_algorithms.Robot_player(x_center, y_center, 0, name="My Robot", team="A",evaluations=evaluations,it_per_evaluation=it_per_evaluation)) # start from left: 4, y_center
     return robots
---
+++ genetic_algorithms.py
@@ -0,0 +1,158 @@
+
+from robot import * 
+import math
+
+nb_robots = 0
+debug = False
+
+class Robot_player(Robot):
+
+    team_name = "Optimizer"
+    robot_id = -1
+    iteration = 0
+
+    param = []
+    bestParam = []
+    it_per_evaluation = 400
+    trial = 0
+
+    
+
+    x_0 = 0
+    y_0 = 0
+    theta_0 = 0 # in [0,360]
+
+    # Nouvelles variables
+    max_strategies = 500;
+
+    old_log_sum_of_translation = 0
+    old_log_sum_of_rotation = 0
+    score = 0
+    total_score = 0
+
+    bestScore = -100000 ; # ex1
+    orientations = []
+
+    subTrial = 0
+
+    parentParam = []
+    parentScore = -100000
+
+    is_first_generation = True
+
+    generation = 0
+    max_generation = 100
+
+    def __init__(self, x_0, y_0, theta_0, name="n/a", team="n/a",evaluations=0,it_per_evaluation=0):
+        global nb_robots
+        self.robot_id = nb_robots
+        nb_robots+=1
+        self.x_0 = x_0
+        self.y_0 = y_0
+        self.orientations = [0, 90, 360]
+        self.theta_0 = self.orientations[0];
+        self.param = [random.randint(-1, 1) for i in range(8)]
+        self.it_per_evaluation = it_per_evaluation
+        self.old_log_sum_of_translation = 0
+        self.old_log_sum_of_rotation = 0
+        self.score = 0
+        self.total_score = 0
+
+        super().__init__(x_0, y_0, theta_0, name=name, team=team)
+
+    def reset(self):
+     
+        super().reset()
+    
+
+
+    def step(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
+
+        #Calcul score
+        effective_translation = self.log_sum_of_translation - self.old_log_sum_of_translation
+        effective_rotation = self.log_sum_of_rotation - self.old_log_sum_of_rotation
+
+        self.score += effective_translation * (1 - abs(effective_rotation))
+
+        self.old_log_sum_of_translation = self.log_sum_of_translation
+        self.old_log_sum_of_rotation = self.log_sum_of_rotation
+
+
+        #Fin évaluation 
+        if self.iteration % self.it_per_evaluation == 0 and self.iteration > 0:
+                
+ 
+                self.total_score += self.score
+                self.subTrial += 1
+                
+
+                #CAS1 S'il y a d'autres orientations à tester 
+                if self.subTrial < len(self.orientations):
+                    self.theta_0 = self.orientations[self.subTrial]
+
+                    self.score = 0
+                    self.old_log_sum_of_rotation = 0
+                    self.old_log_sum_of_translation = 0
+                    self.iteration =0
+                    return 0, 0, True
+                else :
+                    print("Génération  ", self.generation, " : ", self.total_score)
+                    print ("\tparameters           =",self.param)
+                    print ("\ttranslations         =",self.log_sum_of_translation,"; rotations =",self.log_sum_of_rotation) # *effective* translation/rotation (ie. measured from displacement)
+                    print ("\tdistance from origin =",math.sqrt((self.x-self.x_0)**2+(self.y-self.y_0)**2))
+                    print ("\tscore                =",self.score)
+
+                    #Premier parent
+                    if self.is_first_generation:
+                        # Premier essai, on initialise le parent
+                        self.parentParam = self.param[:]
+                        self.parentScore = self.total_score
+                        self.is_first_generation = False
+                    else:
+                        # Comparaison avec parent
+                        if self.total_score > self.parentScore:
+                            # Nouveau parent
+                            self.parentParam = self.param[:]
+                            self.parentScore = self.total_score
+                            print ("||| New parent : ", self.parentScore, " |||")
+                    
+                    if self.generation < self.max_generation:
+                        #Prochaine génération
+                        self.param = self.parentParam[:]  
+                        ind = random.randint(0,7)
+                        values = [-1,0,1]
+                        values.remove(self.param[ind]) 
+                        # Forcement du changement
+                        self.param[ind] = random.choice(values)
+                        self.generation += 1
+
+    
+                    else:
+                        print ("||| End : ")
+                        print ("Best score :", self.parentScore)
+                        print ("Best parameters:", self.parentParam)
+                        #Avec le meilleur individu
+                        self.param = self.parentParam[:]
+                        self.it_per_evaluation = 1000  # On passe à 1000 itérations
+                    
+                    #Reset
+                    self.subTrial = 0
+                    self.theta_0 = self.orientations[0]
+                    self.total_score = 0
+                    self.score = 0
+                    self.old_log_sum_of_rotation = 0
+                    self.old_log_sum_of_translation = 0
+                    self.iteration =0
+                    return 0, 0, True
+
+        # fonction de contrôle (qui dépend des entrées sensorielles, et des paramètres)
+        translation = math.tanh ( self.param[0] + self.param[1] * sensors[sensor_front_left] + self.param[2] * sensors[sensor_front] + self.param[3] * sensors[sensor_front_right] )
+        rotation = math.tanh ( self.param[4] + self.param[5] * sensors[sensor_front_left] + self.param[6] * sensors[sensor_front] + self.param[7] * sensors[sensor_front_right] )
+
+        self.iteration = self.iteration + 1
+        return translation, rotation, False #Reset
+        
+                
+        
+
+      
